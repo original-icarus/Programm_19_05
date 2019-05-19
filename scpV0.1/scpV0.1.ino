de38f8f24ee7fce07eb7cd58cac44477d607c6e0
Niklas Waldmann <n.waldmann@tu-braunschweig.de>
2019-05-19 13:47:18 +0200

    Grundprogramm
    Platzhalter für das spätere Programm

---
+++ scpV0.1/scpV0.1.ino
@@ -0,0 +1,25 @@
+// Programm Definitio: tbc....
+
+
+
+//Funktionsdeklaration:
+  // Funktion_1: Temperatur auslesen
+  // Funktion_2: Lichtstärke auslesen
+  // Funktion_3: Stepper
+  // Funktion_4: Servo
+  // Funktion_5: Luftfeuchtigkeit auslesen
+  // Funktion_6: Displayausgabe
+  // Funktion_7: Ladestatus
+  // Funktion_8: Manuelle Steuerung
+  // Funktion_9: Menü
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
